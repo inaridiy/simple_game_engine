7fa36a8bb316676ae93872904e65a77800f35b02
inaridiy <inainainariinainari@gmail.com>
2021-09-06 22:45:17 +0900

    初期設定

---
+++ webpack.config.js
@@ -0,0 +1,40 @@
+const BundleAnalyzerPlugin =
+  require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
+
+const config = {
+  mode: 'development',
+
+  entry: './src/main.ts',
+
+  module: {
+    rules: [
+      {
+        test: /\.(tsx|ts)$/,
+
+        use: 'ts-loader',
+      },
+    ],
+  },
+
+  resolve: {
+    extensions: ['.ts', '.tsx', '.js', '.json'],
+  },
+  performance: {
+    maxEntrypointSize: 500000,
+    maxAssetSize: 500000,
+  },
+
+  target: ['web', 'es5'],
+
+};
+module.exports = (env, argv) => {
+  console.log(argv);
+  if (argv.mode !== 'production') {
+    config.target = 'web';
+  }
+  if (argv.mode === 'production') {
+    config.plugins.push(new BundleAnalyzerPlugin());
+    
+  }
+  return config;
+};
