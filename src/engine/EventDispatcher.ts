8f05bdc83d6cf3e96572340d9c8c84aeeed53f93
inaridiy <inainainariinainari@gmail.com>
2021-10-25 23:14:32 +0900

    EventDispatherの型関連の変更(仮)

--- src/engine/EventDispatcher.ts
+++ src/engine/EventDispatcher.ts
@@ -1,16 +1,18 @@
+import { SystemEvent } from './types/SystemEvent';
+
 type cb<T> = (event: GameEvent<T>) => void;
 
-export class EventDispatcher<T, S extends string = string> {
-  public _eventListeners: { [t: string]: cb<T>[] } = {};
+export class EventDispatcher<T extends SystemEvent[]> {
+  public _eventListeners: { [t in T[number][0]]?: cb<T>[] } = {};
 
-  on(type: S, callback: cb<T>): void {
+  on(type: T[number][0], callback: cb<T[number][1]>): void {
     if (type in this._eventListeners) {
-      this._eventListeners[type].push(callback);
+      this._eventListeners[type]?.push(callback);
     } else {
       this._eventListeners[type] = [callback];
     }
   }
-  dispatchEvent(type: S, event: GameEvent<T>): void {
+  dispatchEvent(type: T[number][0], event: GameEvent<T>): void {
     const listeners = this._eventListeners[type];
     if (listeners != undefined)
       listeners.forEach((callback) => callback(event));
