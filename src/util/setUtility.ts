b53407da6f1b698bb5ba93412dc7de8a6d836d97
inaridiy <inainainariinainari@gmail.com>
2021-12-01 21:46:25 +0900

    ちょい修正

--- src/util/setUtility.ts
+++ src/util/setUtility.ts
@@ -1,17 +1,5 @@
 export const union = <T>(a: Set<T>, b: Set<T>): Set<T> =>
   new Set(Array.from(a).concat(Array.from(b)));
 
-export const intersection = <T>(a: Set<T>, b: Set<T>): Set<T> => {
-  const prossesSize = Math.max(a.size, b.size);
-  if (prossesSize < 500000) {
-    return new Set([...a].filter((x) => b.has(x)));
-  } else {
-    const _intersection: T[] = [];
-    for (const x of a) {
-      if (b.has(x)) {
-        _intersection.push(x);
-      }
-    }
-    return new Set(_intersection);
-  }
-};
+export const intersection = <T>(a: Set<T>, b: Set<T>): Set<T> =>
+  new Set([...a].filter((x) => b.has(x)));
